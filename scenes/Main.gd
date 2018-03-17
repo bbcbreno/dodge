e2b250dded258588301f804d8cbebd8d560fde74
Breno Brito Caldeira <breno@Brenos-MacBook-Pro.local>
2018-03-17 03:37:52 -0300

    makes touch works properly

--- scenes/Main.gd
+++ scenes/Main.gd
@@ -2,10 +2,36 @@ extends Node
 
 export (PackedScene) var Mob
 var score
+var touch_position = Vector2()
 
 
 func _ready():
 	randomize()
+	
+func _physics_process(delta):
+#	var space_state = get_world_2d().direct_space_state
+#	var result = space_state.intersect_ray(Vector2(150, 150), Vector2(600, 150))
+#	if result:
+#		print("Hit at collider: ", result.collider)
+#		print("Hit at collider_id: ", result.collider_id)
+#		print("Hit at rid: ", result.rid)
+#		print("Hit at point: ", result.position)
+#		print("name: ", result.collider.name)
+#		print("---")
+#		if result.collider.name.find("Mob") == 1:
+#			result.collider.queue_free()
+	if touch_position.x > 0:
+		var space_state = get_world_2d().direct_space_state
+		var result = space_state.intersect_point(touch_position, 1, [], 2)
+		var f = result.front()
+		if f != null:
+			print("pow!")
+			f.collider.queue_free()
+		touch_position = Vector2()
+			
+func _input(event):
+	if event is InputEventScreenTouch and event.pressed:
+		touch_position = event.position
 
 func game_over():
 	$ScoreTimer.stop()
